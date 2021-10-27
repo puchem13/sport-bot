666ab0724329337efb3b29fbf67c3b4d594502c7
sebastian.rieger@dynatrace.com <sebastian.rieger@dynatrace.com>
2021-10-27 21:06:46 +0200

    refactor: rework event checking mehtod
    There is no need to check for the current and the next day when we post
    all events for the next day into the channel.
    Also when the next day is Saturday we check the events for monday and
    post them already on friday into the channel.
    The refactoring also allows to have multiple events on one day and and
    post all of them into the channel.

--- bot.py
+++ bot.py
@@ -24,42 +24,35 @@ def post_message(message):
     client.chat_postMessage(channel='#testing-sport-bot', text=message)
 
 
-def get_message(day):
+def get_messages(day):
+    event_to_send_list = []
     for event in jsonEvents["EVENTS"].items():
         event_name, details = event
         event_day = details["DAY"]
         event_time = details["TIME"]
         print("Current processed event: " + event_name)
-        if (details["ACTIVE"]):
+        if details["ACTIVE"]:
             print("event is active and can be checked")
-            if (event_day == day):
+            if event_day == day:
                 print("we need to announce this event in slack")
-                return (
+                event_to_send_list.append(
                     ":running::woman-running:*Will you be joining "
                     f"the running event on {event_day} at {event_time}?*"
                     f":running_shirt_with_sash:\n"
                     "Please react to this message with :thumbsup: :thumbsdown:"
                     )
-
-
-def check_event(day, startTime, endTime):
-    if day in jsonEvents and not jsonEvents[day]["cancelled"]:
-        eventTime = datetime.strptime(jsonEvents[day]["time"], '%H:%S')
-        if startTime < eventTime.time() and eventTime.time() < endTime:
-            return True
-    return False
+    return event_to_send_list
 
 
 def send_message():
-    TODAY = datetime.now().date().strftime("%A")
-    if check_event(TODAY, time(11, 00), time(23, 59)):
-        MESSAGE = get_message(TODAY)
-        post_message(message=MESSAGE)
-
-    NEXTDAY = (datetime.now() + timedelta(days=1)).strftime("%A")
-    if check_event(NEXTDAY, time(00, 00), time(11, 00)):
-        MESSAGE = get_message(NEXTDAY)
-        post_message(message=MESSAGE)
+    today = datetime.now()
+    tomorrow = today + timedelta(days=1)
+    weekday_to_check = tomorrow.strftime("%A")
+    if weekday_to_check == "Saturday":
+        weekday_to_check = (tomorrow + timedelta(days=2)).strftime("%A")
+    slack_messages = get_messages(weekday_to_check)
+    for message in slack_messages:
+        post_message(message=message)
 
 
 schedule.every().day.at("14:04").do(send_message)
